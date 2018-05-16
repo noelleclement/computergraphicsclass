aa29cfae3def386d989237e71bdc861d12c98ef7
NoelleIsThaBomb.com <noelleclement@hotmail.com>
2018-05-16 13:58:07 +0200

    finished assignment 2

--- ass2/colourconversions.py
+++ ass2/colourconversions.py
@@ -2,42 +2,102 @@
 #computer graphics assignment 2
 
 
+#notes for whole assignment:
+    #rgb cmy sl and alpha all can have values between 0 and 1 (so often conversion isn't necessary in calculations)
+    #h can have a value between 0 and 360
+    #throughout whole assignment I have used the most basic means of calculating and coding the functions
+    #bc of ^ comments weren't really necessary, since it's mainly implementing the formulas
+    #formulas from RapidTables
+
+
 #RGBtoCMY(R, G, B)
-	#bc rgb can have values between 0 and 1 no need for conversion from 0-255 to 0-1 
-	#k(black) = 1-max(r, g, b)
-	#c = (1-r-k) / (1-k)
-	#m = (1-g-k) / (1-k)
-	#y = (1-b-k) / (1-k)
-
 def RGBtoCMY(r, g, b):
-    if (r == 0) and (g == 0) and (b == 0):
-        #black
-        return 0, 0, 0
-
     return (1-r), (1-g), (1-b) 
 
 
 
-#CMYtoRGB(C, M, Y) cyaan (turkoois) magenta yellow
-	#r = 255 x (1-c) x (1-k)
-	#g = 255 x (1-m) x (1-k)
-	#b = 255 x (1-y) x (1-k)
-
-
+#CMYtoRGB(C, M, Y) cyaan magenta yellow
 def CMYtoRGB(c, m, y):
-    return (1-c), (1-m, (1-y)
+    return (1-c), (1-m), (1-y)
 
 
 
 #RGBtoHSL(R, G, B)
+#rgb already 0-1 so no conversion needed
+def RGBtoHSL(r,g,b):
+    cMax = max(r,g,b)
+    cMin = min(r,g,b)
+    cDelta = cMax - cMin
+    l = (cMax+cMin)/2
+
+    if (cDelta == 0):
+        h = 0
+        s = 0
+    elif (cMax == r):
+        h = 60 * (((g-b)/cDelta)%6)
+        s = cDelta/(2-abs(2*l-1))
+    elif (cMax == g):
+        h = 60 * (((b-r)/cDelta)+2)
+        s = cDelta/(2-abs(2*l-1))
+    elif (cMax == b):
+        h = 60 * (((r-g)/cDelta)+4)
+        s = cDelta/(2-abs(2*l-1))
+    else:
+        print ('wrong input in RGBtoHSL')
+
+    return h, s, l
+            
+
+
+#HSLtoRGB(H, S, L)
+def HSLtoRGB(h,s,l):
+    c = (1-abs(2*l-1)) * s
+    x = c*(1-abs((h/60)%2 - 1))
+    m = l-c/2
+
+    if (h>=0 and h<60):
+        r=c
+        g=x
+        b=0
+    elif (h>=60 and h<120):
+        r=x
+        g=c
+        b=0
+    elif (h>=120 and h<180):
+        r=0
+        g=c
+        b=x
+    elif (h>=180 and h<240):
+        r=0
+        g=x
+        b=c
+    elif (h>=240 and h<300):
+        r=x
+        g=0
+        b=c
+    elif (h>=300 and h<360):
+        r=c
+        g=0
+        b=x
+    else:
+        print ('wrong input in HSLtoRGB')
+
+    return (r+m), (g+m), (b+m)
 
 
 
-//HSLtoRGB(H, S, L)
-//transparency(R1, G1, B1, alpha1, R2, G2, B2) of a surface (r1, g1, b1, a1) on top of (r2, g2, b2, 1)
+#transparency(R1, G1, B1, alpha1, R2, G2, B2) of a surface (r1, g1, b1, a1) on top of (r2, g2, b2, 1)
+#(R,G,B)res = a(R,G,B)front + (1-a)(R,G,B)back > alpha of back 1, so in this function not necessary to use whole formula
+def transparency(r1,g1,b1,a1,r2,g2,b2):
+    r1 = a1*r1
+    g1 = a1*g1
+    b1 = a1*b1
+    return (r1+r2), (g1+g2), (b1+b2)
 
-//RGB CMY SL and alpha can have values between 0 and 1
-//H can have value between 0-360 degrees
 
 
-
+print (RGBtoCMY(0.5,0.5,0.5))
+print (CMYtoRGB(1,1,1))
+print (RGBtoHSL(0,1,0.5))
+print (HSLtoRGB(180.0, 0.5, 0.5))
+print (transparency(0,1,1,0.5,1,0.75,0))
